630d40e8e5e5d764fc4b3820d59584834b037c45
PapaGreeen <jacob.wilder826@gmail.com>
2025-05-18 22:06:33 -0400

    implemented working main menu / fixed tilemap issue / still need collision

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -2,3 +2,9 @@ extends Control
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/Level_0.tscn")
+
+func _on_settings_button_pressed():
+	print("Settings screen coming soon!")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
